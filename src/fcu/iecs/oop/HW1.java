c13f630ee51978771af78440df356587ccb68573
Jimmy <Jimmy@Jimmy-PC>
2016-10-11 03:40:16 +0800

    HW1

---
+++ src/fcu/iecs/oop/HW1.java
@@ -0,0 +1,14 @@
+package fcu.iecs.oop;
+
+public class HW1 {
+
+	public static void main(String[] args) {
+		
+		String lyrics="Let it go! Let it go! Cannot hold it back anymore";
+		
+		lyrics=lyrics.replace("it","\"her\"");
+		System.out.println(lyrics.replace("Cannot","Can't") );
+
+	}
+
+}
